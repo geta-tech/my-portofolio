3ae8e2e59d3927d671a7b23bfd06e301341eacc0
geta-tech <rethasilalahi9@gmail.com>
2026-02-26 13:19:44 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,113 @@
+const myPortfolioData = {
+    projects: [
+        {
+            id: "p1",
+            title: "Website Donasi Tracker",
+            tech: "PHP, JavaScript, MySQL",
+            thumbnail: "assets/images/projek-utama-1.png", 
+            description: "Proyek Donation Tracker adalah sebuah sistem berbasis web yang dirancang untuk mempermudah pengelolaan dan pelacakan donasi. Sistem ini bertujuan untuk meningkatkan efisiensi, akurasi, dan transparansi dalam pengelolaan dana sosial yang dikumpulkan oleh organisasi atau individu. Dengan memanfaatkan teknologi digital, Donation Tracker diharapkan dapat menjadi solusi yang relevan dalam menghadapi tantangan pengelolaan donasi secara manual yang sering kali memakan waktu dan rentan terhadap kesalahan.",
+            evidence: [
+                "assets/images/grid-projek-1.png",   
+                "assets/images/grid-projek-1.1.png" 
+            ]
+        },
+        {
+            id: "p2",
+            title: "Machine Learning IKK Provinsi",
+            tech: "Python, Streamlit",
+            thumbnail: "assets/images/projek-utama-2.png",
+            description: "Proyek ini saya kerjakan bersama teman saya berbasis web yang memanfaatkan Machine Learning untuk menganalisis dan memprediksi IKK pada berbagai provinsi di Indonesia. Sistem ini dibangun menggunakan Python dan Streamlit untuk menampilkan hasil analisis data secara interaktif, sehingga membantu pengguna memahami pola data.",
+            evidence: [
+                "assets/images/grid-projek-2.png"
+            ]
+        },
+
+        {
+            id: "p3",
+            title: "Manajemen Sewa Kazma Kost",
+            tech: "Visual basic.net, MySQL",
+            thumbnail: "assets/images/projek-utama-3.png", 
+            description: "Pembuatan aplikasi ini adalah untuk mempermudah pengelolaan kost secara digital, khususnya dalam hal pencatatan data kamar, penghuni, serta transaksi pembayaran bulanan supaya pemilik kost tidak perlu lagi  melakukan pencatatan manual. ",
+            evidence: [
+                "assets/images/grid-projek-3.png"
+            ]
+        },
+
+        {
+            id: "p4",
+            title: "BIF (Bakrie Information Facility)",
+            tech: "Dart, Java, MySQL, Android Studio, Figma",
+            thumbnail: "assets/images/projek-utama-4.png", 
+            description: "Proyek ini saya kerjakan bersama teman saya bahwa BIF adalah aplikasi mobile berbasis Flutter yang dirancang untuk membantu sivitas akademika Universitas Bakrie dalam mengakses dan mengelola penyewaan fasilitas kampus secara terpusat. ",
+            evidence: [
+                "assets/images/grid-projek-4.png"
+            ]
+        }
+    ],
+
+    skills: [
+    { name: "HTML", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" },
+    { name: "JavaScript", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" },
+    { name: "Python", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" },
+    { name: "MySQL", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg" },
+    { name: "Tailwind", logo: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg" },
+    { name: "Linux", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg" },
+    { name: "Dart", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dart/dart-original.svg" },
+    { name: "Vb.Net", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/visualstudio/visualstudio-plain.svg" }
+    ],
+
+experience: [
+    { 
+        year: "2025 - SEKARANG", 
+        role: "Himpunan Mahasiswa Sistem Informasi", 
+        company: "Secretary Division Academic", 
+        desc: "Mengelola administrasi dan dokumentasi kegiatan akademik serta mengoordinasikan program kerja divisi. ",
+        image: "assets/images/experience-1.png",
+        fullDesc: "Sebagai Sekretaris Divisi Akademik, saya bertanggung jawab dalam mengelola proses administrasi dan dokumentasi kegiatan akademik organisasi. Saya menyusun notulensi rapat, mengatur jadwal kegiatan, mengelola surat-menyurat, serta memastikan seluruh program kerja divisi berjalan sesuai perencanaan." 
+    },
+
+    { 
+        year: "2025 - SEKARANG", 
+        role: "IEEE SB Bakrie University", 
+        company: "Public Relations Staff", 
+        desc: "Mengelola komunikasi eksternal dan promosi kegiatan organisasi melalui media digital. ",
+        image: "assets/images/experience-2.png",
+        fullDesc: "Sebagai Public Relations Staff, saya bertanggung jawab dalam mengelola komunikasi eksternal organisasi serta membangun hubungan dengan mitra dan audiens." 
+    },
+
+     { 
+        year: "2025 - SEKARANG", 
+        role: " Wo-Men in Tech Security", 
+        company: "Community Engagement Staff", 
+        desc: "Membangun dan meningkatkan keterlibatan komunitas melalui program, event, dan komunikasi yang aktif.",
+        image: "assets/images/experience-3.png",
+        fullDesc: "Sebagai Community Engagement, saya berperan dalam membangun hubungan yang kuat antar anggota komunitas serta meningkatkan partisipasi dalam berbagai program dan kegiatan." 
+    },
+
+     { 
+        year: "November 2025", 
+        role: " Pengabdian Masyarakat SMP 101 Jakarta", 
+        company: " Leader ", 
+        desc: " Internalisasi Nilai Nilai Pancasila Dengan Aksi Nyata Di Lingkungan.",
+        image: "assets/images/experience-4.png",
+        fullDesc: "Saya dan bersama teman saya melakukan pengabdian kepada masyarakat yang tujuannya untuk meningkatkan pemahaman siswa mengenai nilai nilai Pancasila serta menumbuhkan sikap gotong royong, toleransi dan nasionalisme melalui praktik di ligkungan sekolah." 
+    },
+
+    { 
+        year: "November 2025", 
+        role: " Seminar CyberSecurity UBSI Jatiwaringin", 
+        company: " Narasumber ", 
+        desc: " Fundamental cybersecurity : Principles and best practices.",
+        image: "assets/images/experience-5.png",
+        fullDesc: "Saya memberikan pemahaman tentang social engineering yang merupakan teknik manipulasi psikologis untuk memengaruhi seseorang agar memberikan informasi, mengklik sesuatu, atau melakukan tindakan tertentu yang seharusnya tidak mereka lakukan. Jenis jenisnya adalah Phishing, Smishing, Vishing." 
+    },
+
+],
+
+    education: [
+        {
+            degree: "S1 Sistem Informasi",
+            school: "Universitas Bakrie"
+        }
+    ]
+};
